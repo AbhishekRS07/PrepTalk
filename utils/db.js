6e4082a66f45fbe09f45138aa65f9a0ec2f4099b
Abhishek Raj Singh <abhishekrajsingh@Abhisheks-MacBook-Air-2.local>
2026-04-03 13:17:17 +0530

    added missing field

--- utils/db.js
+++ utils/db.js
@@ -2,5 +2,5 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import * as schema from "./schema";
 
-const client = postgres(process.env.DRIZZLE_DB_URL, { max: 1 });
+const client = postgres(process.env.DRIZZLE_DB_URL, { max: 1, ssl: "require" });
 export const db = drizzle(client, { schema });
